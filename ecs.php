2bfbfe185d562f37a6741521de82859953775e0f
Karlo Mikuš <karlo.mikus1@gmail.com>
2022-11-27 13:37:52 +0100

    Add easy coding standard

---
+++ ecs.php
@@ -0,0 +1,10 @@
+<?php
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([__DIR__ . '/app']);
+
+    $ecsConfig->sets([SetList::PSR_12]);
+};
