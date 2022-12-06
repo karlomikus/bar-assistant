57a6e6537375c0f01728ecff677cc2663c7a7479
Karlo Mikuš <karlo.mikus1@gmail.com>
2022-12-06 21:42:09 +0100

    Add missing method call

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -50,7 +50,7 @@ class ImageController extends Controller
         [$content, $etag] = Cache::remember('image_thumb_' . $id, 1 * 24 * 60 * 60, function () use ($id) {
             $dbImage = Image::findOrFail($id);
             $disk = Storage::disk('app_images');
-            $responseContent = (string) ImageManagerStatic::make($disk->get($dbImage->file_path))->fit(200, 200);
+            $responseContent = (string) ImageManagerStatic::make($disk->get($dbImage->file_path))->fit(200, 200)->encode();
             $etag = md5($dbImage->id . '-' . $dbImage->updated_at->format('Y-m-d H:i:s'));
 
             return [$responseContent, $etag];
