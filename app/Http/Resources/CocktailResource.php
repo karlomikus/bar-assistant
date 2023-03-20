b2e2a8049e826122f50a9814026a6cc6cdb7d836
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-03-20 20:32:35 +0100

    Add cocktail public id to response

--- app/Http/Resources/CocktailResource.php
+++ app/Http/Resources/CocktailResource.php
@@ -28,6 +28,7 @@ class CocktailResource extends JsonResource
             'description' => e($this->description),
             'source' => $this->source,
             'has_public_link' => $this->public_id !== null,
+            'public_id' => $this->public_id,
             'main_image_id' => $this->images->sortBy('sort')->first()->id ?? null,
             'images' => ImageResource::collection($this->images),
             'tags' => $this->tags->pluck('name'),
