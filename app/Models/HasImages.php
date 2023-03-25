3aae4a5491f26663127668a9a935dbe41addeb42
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-03-25 17:32:00 +0100

    Update cs

--- app/Models/HasImages.php
+++ app/Models/HasImages.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Kami\Cocktail\Models;
 
+use LogicException;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\InvalidCastException;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use LogicException;
+use Illuminate\Database\Eloquent\InvalidCastException;
 
 trait HasImages
 {
