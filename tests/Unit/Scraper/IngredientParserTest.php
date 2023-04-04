3e602b3e33e7bc6ddf19b78554fbe0d3f5589c71
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-04-04 22:32:14 +0200

    Fix namespace

--- tests/Unit/Scraper/IngredientParserTest.php
+++ tests/Unit/Scraper/IngredientParserTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
+namespace Tests\Unit\Scraper;
 
 use Tests\TestCase;
 use Kami\Cocktail\Scraper\IngredientParser;
--- tests/Unit/Services/ImageServiceTest.php
+++ tests/Unit/Services/ImageServiceTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
+namespace Tests\Unit\Services;
 
 use Tests\TestCase;
 use Illuminate\Log\LogManager;
