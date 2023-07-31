802e7da63e272aa8aef322d81c90e15910f7bba5
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-07-31 21:22:01 +0200

    Add total collections count

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -11,6 +11,7 @@ use Kami\Cocktail\Models\Cocktail;
 use Kami\Cocktail\Models\Ingredient;
 use Kami\Cocktail\Models\UserIngredient;
 use Kami\Cocktail\Services\CocktailService;
+use Kami\Cocktail\Models\Collection as CocktailCollection;
 
 class StatsController extends Controller
 {
@@ -42,6 +43,7 @@ class StatsController extends Controller
         $stats['total_shelf_ingredients'] = UserIngredient::where('user_id', $request->user()->id)->count();
         $stats['most_popular_ingredients'] = $popularIngredientIds;
         $stats['top_rated_cocktails'] = $topRatedCocktailIds;
+        $stats['total_collections'] = CocktailCollection::where('user_id', $request->user()->id)->count();
 
         return response()->json(['data' => $stats]);
     }
