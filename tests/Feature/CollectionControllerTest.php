bed40e155434c927ecfbd08c9bcd1a2075519b41
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-08-27 20:55:38 +0200

    Update collection tests

--- tests/Feature/CollectionControllerTest.php
+++ tests/Feature/CollectionControllerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Kami\Cocktail\Models\Bar;
 use Kami\Cocktail\Models\User;
 use Kami\Cocktail\Models\Cocktail;
 use Kami\Cocktail\Models\Collection;
@@ -24,11 +25,13 @@ class CollectionControllerTest extends TestCase
 
     public function test_list_user_collections_response()
     {
+        $this->setupBar();
+
         Collection::factory()->count(10)->create([
-            'user_id' => auth()->user()->id,
+            'bar_membership_id' => 1,
         ]);
 
-        $response = $this->getJson('/api/collections/');
+        $response = $this->getJson('/api/collections?bar_id=1');
 
         $response->assertOk();
         $response->assertJson(
@@ -41,10 +44,12 @@ class CollectionControllerTest extends TestCase
 
     public function test_show_user_collection_response()
     {
+        $this->setupBar();
+
         $collection = Collection::factory()->create([
             'name' => 'TEST',
             'description' => 'Description',
-            'user_id' => auth()->user()->id,
+            'bar_membership_id' => 1,
         ]);
 
         $response = $this->getJson('/api/collections/' . $collection->id);
@@ -64,9 +69,13 @@ class CollectionControllerTest extends TestCase
 
     public function test_create_collection_response()
     {
-        $response = $this->postJson('/api/collections/', [
+        $bar = $this->setupBar();
+
+        $cocktail = Cocktail::factory()->create(['bar_id' => $bar->id]);
+        $response = $this->postJson('/api/collections?bar_id=1', [
             'name' => 'TEST',
             'description' => 'Description',
+            'cocktails' => [$cocktail->id]
         ]);
 
         $response->assertCreated();
@@ -78,17 +87,46 @@ class CollectionControllerTest extends TestCase
                 ->has('data.id')
                 ->where('data.name', 'TEST')
                 ->where('data.description', 'Description')
-                ->where('data.cocktails', [])
+                ->where('data.cocktails', [$cocktail->id])
+                ->etc()
+        );
+    }
+
+    public function test_create_collection_does_not_add_cocktail_from_another_bar_response()
+    {
+        $bar = $this->setupBar();
+        $anotherBar = Bar::factory()->create(['created_user_id' => auth()->user()->id]);
+
+        $cocktail1 = Cocktail::factory()->create(['bar_id' => $bar->id]);
+        $cocktail2 = Cocktail::factory()->create(['bar_id' => $anotherBar->id]);
+        $response = $this->postJson('/api/collections?bar_id=1', [
+            'name' => 'TEST',
+            'description' => 'Description',
+            'cocktails' => [$cocktail1->id, $cocktail2->id]
+        ]);
+
+        $response->assertCreated();
+        $this->assertNotEmpty($response->headers->get('Location'));
+        $response->assertJson(
+            fn (AssertableJson $json) =>
+            $json
+                ->has('data')
+                ->has('data.id')
+                ->where('data.name', 'TEST')
+                ->where('data.description', 'Description')
+                ->where('data.cocktails', [$cocktail1->id])
                 ->etc()
         );
     }
 
     public function test_update_collections_response()
     {
+        $this->setupBar();
+
         $model = Collection::factory()->create([
             'name' => 'TEST',
             'description' => 'Description',
-            'user_id' => auth()->user()->id,
+            'bar_membership_id' => 1,
         ]);
 
         $response = $this->putJson('/api/collections/' . $model->id, [
@@ -111,10 +149,12 @@ class CollectionControllerTest extends TestCase
 
     public function test_delete_collection_response()
     {
+        $this->setupBar();
+
         $model = Collection::factory()->create([
             'name' => 'TEST',
             'description' => 'Description',
-            'user_id' => auth()->user()->id,
+            'bar_membership_id' => 1,
         ]);
 
         $response = $this->delete('/api/collections/' . $model->id);
@@ -126,11 +166,13 @@ class CollectionControllerTest extends TestCase
 
     public function test_add_cocktail_to_collection()
     {
-        $cocktail = Cocktail::factory()->create();
+        $bar = $this->setupBar();
+
+        $cocktail = Cocktail::factory()->create(['bar_id' => $bar->id]);
         $collection = Collection::factory()->create([
             'name' => 'TEST',
             'description' => 'Description',
-            'user_id' => auth()->user()->id,
+            'bar_membership_id' => 1,
         ]);
 
         $response = $this->putJson('/api/collections/' . $collection->id . '/cocktails/' . $cocktail->id);
