c073f250fcb736419b22bd6aaff301f78f58c8e3
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-08-29 22:37:49 +0200

    Fix null check

--- app/Policies/CocktailPolicy.php
+++ app/Policies/CocktailPolicy.php
@@ -14,7 +14,7 @@ class CocktailPolicy
 
     public function before(User $user, string $ability): bool|null
     {
-        if ($user->isBarAdmin(bar()->id)) {
+        if (bar()->id && $user->isBarAdmin(bar()->id)) {
             return true;
         }
 
