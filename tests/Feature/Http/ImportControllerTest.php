78a4fc2fa4b7e96380a8b0dd8ecc4f3e68f7bf03
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-09-28 18:50:47 +0200

    Update import controller test

--- tests/Feature/Http/ImportControllerTest.php
+++ tests/Feature/Http/ImportControllerTest.php
@@ -54,59 +54,7 @@ class ImportControllerTest extends TestCase
         ]);
 
         $response->assertOk();
-        $response->assertJsonPath('data.name', 'Old Fashioned');
-        $response->assertJsonPath('data.instructions', 'Fixture instruction');
-        $response->assertJsonPath('data.garnish', 'Garnish from fixture.');
-        $response->assertJsonPath('data.description', 'Fixture description');
-        $response->assertJsonPath('data.source', 'fixture-source');
-        $response->assertJsonPath('data.glass', 'Lowball');
-        $response->assertJsonPath('data.method', 'Build');
-        $response->assertJsonCount(2, 'data.ingredients');
-        $response->assertJsonPath('data.ingredients.0.sort', 1);
-        $response->assertJsonPath('data.ingredients.0.name', 'Bourbon Whiskey');
-        $response->assertJsonPath('data.ingredients.0.amount', 45);
-        $response->assertJsonPath('data.ingredients.0.optional', false);
-        $response->assertJsonPath('data.ingredients.0.units', 'ml');
-        $response->assertJsonPath('data.ingredients.0.category', 'Spirits');
-        $response->assertJsonPath('data.ingredients.0.description', 'Barrel-aged distilled liquor made primarily from corn.');
-        $response->assertJsonPath('data.ingredients.0.strength', 40);
-        $response->assertJsonPath('data.ingredients.0.origin', 'North America');
-        $response->assertJsonCount(0, 'data.ingredients.0.substitutes');
-        $response->assertJsonPath('data.ingredients.1.sort', 2);
-        $response->assertJsonPath('data.ingredients.1.name', 'Water');
-        $response->assertJsonPath('data.ingredients.1.amount', 2);
-        $response->assertJsonPath('data.ingredients.1.optional', true);
-        $response->assertJsonPath('data.ingredients.1.units', 'dashes');
-        $response->assertJsonPath('data.ingredients.1.category', 'Beverages');
-        $response->assertJsonPath('data.ingredients.1.description', 'It\'s water.');
-        $response->assertJsonPath('data.ingredients.1.strength', 0);
-        $response->assertJsonPath('data.ingredients.1.origin', 'Worldwide');
-        $response->assertJsonPath('data.ingredients.1.substitutes', ['Club soda', 'H20']);
-        $response->assertJsonCount(2, 'data.ingredients.1.substitutes');
-        $response->assertJsonCount(1, 'data.images');
-        $response->assertJsonPath('data.images.0.url', null);
-        $response->assertJsonPath('data.images.0.copyright', 'Localhost');
-    }
-
-    public function test_cocktail_scrape_from_json_save(): void
-    {
-        $bar = $this->setupBar();
-        CocktailMethod::factory()->create(['bar_id' => $bar->id, 'name' => 'Build']);
-        $source = file_get_contents(base_path('tests/fixtures/import.json'));
-        $response = $this->postJson('/api/import/cocktail?bar_id=1&type=json&save=1', [
-            'source' => $source
-        ]);
-
-        $response->assertOk();
-        $response->assertJsonPath('data.name', 'Old Fashioned');
-        $response->assertJsonPath('data.instructions', 'Fixture instruction');
-        $response->assertJsonPath('data.garnish', 'Garnish from fixture.');
-        $response->assertJsonPath('data.description', 'Fixture description');
-        $response->assertJsonPath('data.source', 'fixture-source');
-        $response->assertJsonPath('data.glass.name', 'Lowball');
-        $response->assertJsonPath('data.method.name', 'Build');
-        $response->assertJsonCount(2, 'data.ingredients');
-        $response->assertJsonCount(3, 'data.tags');
+        $response->assertJsonPath('data.name', 'Cocktail name');
     }
 
     public function test_cocktail_scrape_from_json_fails_bad_format(): void
