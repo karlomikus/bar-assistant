f6908df5f57e808a6c67c636a5a8dc60436a9739
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-10-02 20:18:42 +0200

    Add full backup command

---
+++ app/Export/FullBackupToZip.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kami\Cocktail\Export;
+
+use ZipArchive;
+use Carbon\Carbon;
+use Illuminate\Log\LogManager;
+use Illuminate\Support\Facades\File;
+use Kami\Cocktail\Exceptions\ExportException;
+
+class FullBackupToZip
+{
+    public function __construct(
+        private readonly LogManager $log,
+    ) {
+    }
+
+    public function process(?string $exportPath = null): string
+    {
+        $version = config('bar-assistant.version');
+        $meta = [
+            'version' => $version,
+            'date' => Carbon::now()->toJSON(),
+            'called_from' => __CLASS__,
+        ];
+
+        $zip = new ZipArchive();
+
+        File::ensureDirectoryExists(storage_path('bar-assistant/backups'));
+
+        $filename = storage_path(sprintf('bar-assistant/backups/%s_%s.zip', Carbon::now()->format('Ymdhi'), 'bass-backup'));
+        if ($exportPath) {
+            $filename = $exportPath;
+        }
+
+        if ($zip->open($filename, ZipArchive::CREATE) !== true) {
+            $message = sprintf('Error creating zip archive with filepath "%s"', $filename);
+            $this->log->error($message);
+
+            throw new ExportException($message);
+        }
+
+        $zip->addGlob(storage_path('bar-assistant/*.sqlite'), options: ['remove_path' => storage_path('bar-assistant')]);
+        $zip->addGlob(storage_path('bar-assistant/uploads/*/*/*'), options: ['remove_path' => storage_path('bar-assistant')]);
+
+        if ($metaContent = json_encode($meta)) {
+            $zip->addFromString('_meta.json', $metaContent);
+        }
+
+        $zip->close();
+
+        return $filename;
+    }
+}
