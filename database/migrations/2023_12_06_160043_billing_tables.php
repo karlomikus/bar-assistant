8a524027cd9d70875b497281d4a111699b912938
Karlo Mikuš <karlo.mikus1@gmail.com>
2023-12-06 19:15:32 +0100

    Add billing tables

---
+++ database/migrations/2023_12_06_160043_billing_tables.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('billable_id');
+            $table->string('billable_type');
+            $table->string('paddle_id')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamps();
+
+            $table->index(['billable_id', 'billable_type']);
+        });
+
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('billable_id');
+            $table->string('billable_type');
+            $table->string('type');
+            $table->string('paddle_id')->unique();
+            $table->string('status');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('paused_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
+
+            $table->index(['billable_id', 'billable_type']);
+        });
+
+        Schema::create('subscription_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscription_id');
+            $table->string('product_id');
+            $table->string('price_id');
+            $table->string('status');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->unique(['subscription_id', 'price_id']);
+        });
+
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('billable_id');
+            $table->string('billable_type');
+            $table->string('paddle_id')->unique();
+            $table->string('paddle_subscription_id')->nullable()->index();
+            $table->string('invoice_number')->nullable();
+            $table->string('status');
+            $table->string('total');
+            $table->string('tax');
+            $table->string('currency', 3);
+            $table->timestamp('billed_at');
+            $table->timestamps();
+
+            $table->index(['billable_id', 'billable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('subscription_items');
+        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('customers');
+    }
+};
