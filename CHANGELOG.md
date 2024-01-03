afc9e52e61acac2d5b8a8087ef526b754615d487
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-01-03 16:23:24 +0100

    Add csv export

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,13 @@
 - Added `use_parent_as_substitute` setting to bar memberships
     - With this you can toggle using parent ingredient of a specific ingredient as a substitute in your shelf
     - Before, this was available via experimental `PARENT_INGREDIENT_SUBSTITUTE` env variable
+- Added `csv` export to cocktail collections
 
 ## Changes
 - Removed `PARENT_INGREDIENT_SUBSTITUTE` env variable
 - Update login error message depending on mail confirmation config
 - Update min log level to "warning"
+- Updated cocktail markdown export format
 
 # v3.6.0
 ## New
---
+++ app/Export/CocktailsToCSV.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kami\Cocktail\Export;
+
+use League\Csv\Writer;
+use League\Csv\ColumnConsistency;
+use Illuminate\Support\Collection;
+use Kami\Cocktail\Models\Cocktail;
+
+class CocktailsToCSV
+{
+    /**
+     * @param Collection<int, Cocktail> $cocktails
+     */
+    public function process(Collection $cocktails): string
+    {
+        $header = ['id', 'name', 'instructions', 'ingredients', 'garnish'];
+
+        $validator = new ColumnConsistency();
+        $csv = Writer::createFromString();
+        $csv->setEndOfLine("\r\n");
+        $csv->addValidator($validator, 'column_consistency');
+        $csv->setEscape('');
+
+        $csv->insertOne($header);
+        $csv->insertAll($cocktails->map(function (Cocktail $cocktail) {
+            return [
+                $cocktail->slug,
+                $cocktail->name,
+                $cocktail->instructions,
+                $cocktail->ingredients->map(fn($i) => $i->printIngredient())->implode("\n"),
+                $cocktail->garnish,
+            ];
+        }));
+
+        return $csv->toString();
+    }
+}
