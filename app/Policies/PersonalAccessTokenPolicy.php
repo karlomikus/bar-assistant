b66fd0d95a19f5daef4c2429a45a3077789d2970
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-02-10 13:02:42 +0100

    Add missing delete token policy

--- app/Policies/PersonalAccessTokenPolicy.php
+++ app/Policies/PersonalAccessTokenPolicy.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kami\Cocktail\Policies;
 
 use Kami\Cocktail\Models\User;
+use Kami\Cocktail\Models\PersonalAccessToken;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class PersonalAccessTokenPolicy
@@ -14,5 +15,10 @@ class PersonalAccessTokenPolicy
     public function create(User $user): bool
     {
         return $user->hasActiveSubscription();
+    }
+
+    public function delete(User $user, PersonalAccessToken $token): bool
+    {
+        return $user->tokens->contains('id', $token->id);
     }
 }
