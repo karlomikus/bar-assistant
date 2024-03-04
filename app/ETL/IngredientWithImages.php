2214732709c59fd1e17e78499b8aa6296473b718
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-03-04 20:03:06 +0100

    Add ingredient ETL class

---
+++ app/ETL/IngredientWithImages.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kami\Cocktail\ETL;
+
+use JsonSerializable;
+use Illuminate\Support\Str;
+use Kami\Cocktail\Models\Image as ImageModel;
+use Kami\Cocktail\Models\Ingredient as IngredientModel;
+
+readonly class IngredientWithImages implements JsonSerializable
+{
+    /**
+     * @param array<Image> $images
+     */
+    public function __construct(
+        public string $id,
+        public string $name,
+        public ?string $parentId = null,
+        public float $strength = 0.0,
+        public ?string $description = null,
+        public ?string $origin = null,
+        public ?string $color = null,
+        public ?string $category = null,
+        public ?string $createdAt = null,
+        public ?string $updatedAt = null,
+        public array $images = [],
+    ) {
+    }
+
+    public static function fromModel(IngredientModel $model): self
+    {
+        $images = $model->images->map(function (ImageModel $image) {
+            return Image::fromModel($image);
+        })->toArray();
+
+        return new self(
+            Str::slug($model->name),
+            $model->name,
+            $model->parent_ingredient_id ? Str::slug($model->parentIngredient->name) : null,
+            $model->strength,
+            $model->description,
+            $model->origin,
+            $model->color,
+            $model->category?->name ?? null,
+            $model->created_at?->toDateTimeString(),
+            $model->updated_at?->toDateTimeString(),
+            $images,
+        );
+    }
+
+    public static function fromArray(array $sourceArray): self
+    {
+        $images = [];
+        foreach ($sourceArray['images'] ?? [] as $sourceImage) {
+            $images[] = Image::fromArray($sourceImage);
+        }
+
+        return new self(
+            $sourceArray['_id'],
+            $sourceArray['name'],
+            $sourceArray['_parent_id'],
+            $sourceArray['strength'] ?? 0.0,
+            $sourceArray['description'] ?? null,
+            $sourceArray['origin'] ?? null,
+            $sourceArray['color'] ?? null,
+            $sourceArray['category'] ?? null,
+            $sourceArray['created_at'] ?? null,
+            $sourceArray['updated_at'] ?? null,
+            $images,
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            '_id' => $this->id,
+            '_parent_id' => $this->parentId,
+            'name' => $this->name,
+            'strength' => $this->strength,
+            'description' => $this->description,
+            'origin' => $this->origin,
+            'color' => $this->color,
+            'category' => $this->category,
+            'created_at' => $this->createdAt,
+            'updated_at' => $this->updatedAt,
+            'images' => array_map(fn ($model) => $model->toArray(), $this->images),
+        ];
+    }
+
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
+    }
+}
