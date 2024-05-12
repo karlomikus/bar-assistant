59d4047c9c8e11e5ce35a2d57d8150de8d88000c
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-05-12 22:00:16 +0200

    Add unit conversion methods

--- app/Models/CocktailIngredient.php
+++ app/Models/CocktailIngredient.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Kami\Cocktail\Models;
 
+use Kami\RecipeUtils\Converter;
 use Illuminate\Database\Eloquent\Model;
+use Kami\RecipeUtils\UnitConverter\Units;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -41,6 +43,28 @@ class CocktailIngredient extends Model
     public function substitutes(): HasMany
     {
         return $this->hasMany(CocktailIngredientSubstitute::class);
+    }
+
+    public function getAmountAsUnit(Units $toUnits): float
+    {
+        if ($fromUnits = Units::tryFrom($this->units)) {
+            return Converter::fromTo($this->amount, $fromUnits, $toUnits);
+        }
+
+        return $this->amount;
+    }
+
+    public function getMaxAmountAsUnit(Units $toUnits): ?float
+    {
+        if (!$this->amount_max) {
+            return null;
+        }
+
+        if ($fromUnits = Units::tryFrom($this->units)) {
+            return Converter::fromTo($this->amount_max, $fromUnits, $toUnits);
+        }
+
+        return $this->amount_max;
     }
 
     public function printIngredient(): string
