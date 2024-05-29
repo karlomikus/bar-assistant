cac394a87289131b0143f0c92f953e4b34610485
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-05-29 17:36:45 +0200

    Add laravel prompts

--- app/Console/Commands/BarMergeIngredient.php
+++ app/Console/Commands/BarMergeIngredient.php
@@ -6,6 +6,8 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Kami\Cocktail\Models\Ingredient;
+use function Laravel\Prompts\text;
+use function Laravel\Prompts\multiselect;
 
 class BarMergeIngredient extends Command
 {
@@ -14,14 +16,14 @@ class BarMergeIngredient extends Command
      *
      * @var string
      */
-    protected $signature = 'bar:merge-ingredients';
+    protected $signature = 'bar:merge-ingredients {barId} {toIngredientId}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Merge multiple ingredients into one';
 
     /**
      * Execute the console command.
@@ -32,10 +34,27 @@ class BarMergeIngredient extends Command
             throw new \Exception('Not ready yet...');
         }
 
-        $listOfIngredientsToMerge = array_unique([
-            5143, 5924, 6049, 6197, 6204, 6225, 6604, 6866
-        ]);
-        $mergeToIngredient = Ingredient::findOrFail(5122);
+        $barId = $this->argument('barId');
+        $toIngredientId = $this->argument('toIngredientId');
+
+        try {
+            $mergeToIngredient = Ingredient::findOrFail($toIngredientId);
+        } catch (\Throwable) {
+            $this->error('Cannot find ingredient with ID: ' . $toIngredientId);
+
+            return Command::FAILURE;
+        }
+
+        $searchTerm = text('Search for ingredients to merge into "' . $mergeToIngredient->name . '"', required: true);
+
+        $ingredientChoices = DB::table('ingredients')->where('bar_id', $barId)->where('name', 'like', '%' . $searchTerm . '%')->get();
+
+        $listOfIngredientsToMerge = multiselect(
+            label: 'What ingredients do you want to merge?',
+            options: $ingredientChoices->pluck('name', 'id'),
+            scroll: 15,
+            required: true,
+        );
 
         $numberOfChanges = DB::table('cocktail_ingredients')
             ->whereIn('ingredient_id', $listOfIngredientsToMerge)
@@ -44,15 +63,24 @@ class BarMergeIngredient extends Command
             ->count();
 
         $this->line('This will remove the following ingredients:');
-        foreach (Ingredient::findOrFail($listOfIngredientsToMerge) as $ing) {
-            $this->line('   ꞏ ' . $ing->name);
+        $possibleIngredients = Ingredient::find($listOfIngredientsToMerge);
+        if ($possibleIngredients->isEmpty()) {
+            $this->error('Cannot find find any ingredients to merge.');
+
+            return Command::FAILURE;
+        }
+
+        $possibleIngredients = $possibleIngredients->filter(fn ($i) => (int) $i->id !== (int) $toIngredientId);
+
+        foreach ($possibleIngredients as $ing) {
+            $this->line('   ・ ' . $ing->name);
         }
 
         $this->line(sprintf('This will change %s cocktail recipes', $numberOfChanges));
         $this->info(sprintf('CHANGE ingredients to "%s"', $mergeToIngredient->name));
         $this->warn(sprintf('DELETE %s ingredients', count($listOfIngredientsToMerge)));
 
-        if (!$this->confirm('Continue?')) {
+        if (!$this->confirm('Confirm merge?')) {
             return Command::INVALID;
         }
 
@@ -61,7 +89,7 @@ class BarMergeIngredient extends Command
         ]);
 
         // Delete orphan ingredients
-        DB::table('ingredients')->whereIn('id', $listOfIngredientsToMerge)->delete();
+        DB::table('ingredients')->whereIn('id', $possibleIngredients->pluck('id'))->delete();
 
         $this->info('Finished!');
 
