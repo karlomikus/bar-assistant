0f919613f56b7c6714a775474de392e400331e14
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-06-07 21:23:40 +0200

    Add migration

---
+++ database/migrations/2024_06_01_135855_create_complex_ingredients_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('complex_ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('main_ingredient_id')->constrained('ingredients')->onDelete('cascade');
+            $table->foreignId('ingredient_id')->constrained('ingredients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complex_ingredients');
+    }
+};
