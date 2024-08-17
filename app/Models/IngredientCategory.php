3a8c7eacff5fb57aae385ed1655da1389e1bde70
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-08-17 15:34:52 +0200

    Add nested set support to ingredient categories

--- app/Models/IngredientCategory.php
+++ app/Models/IngredientCategory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kami\Cocktail\Models;
 
+use Kalnoy\Nestedset\NodeTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Kami\Cocktail\Models\Concerns\HasBarAwareScope;
@@ -14,6 +15,7 @@ class IngredientCategory extends Model
     /** @use \Illuminate\Database\Eloquent\Factories\HasFactory<\Database\Factories\IngredientCategoryFactory> */
     use HasFactory;
     use HasBarAwareScope;
+    use NodeTrait;
 
     /**
      * @return HasMany<Ingredient>
@@ -21,5 +23,10 @@ class IngredientCategory extends Model
     public function ingredients(): HasMany
     {
         return $this->hasMany(Ingredient::class);
+    }
+
+    protected function getScopeAttributes(): array
+    {
+        return ['bar_id'];
     }
 }
---
+++ database/migrations/2024_08_17_123305_add_nested_set.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('ingredient_categories', function (Blueprint $table) {
+            $table->nestedSet();
+        });
+
+        $bars = \Illuminate\Support\Facades\DB::table('bars')->pluck('id');
+        foreach ($bars as $barId) {
+            \Kami\Cocktail\Models\IngredientCategory::scoped(['bar_id' => $barId])->fixTree();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('ingredient_categories', function (Blueprint $table) {
+            $table->dropNestedSet();
+        });
+    }
+};
