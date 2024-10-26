305452181b1dea48623f7274f4558a665e14471e
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-10-26 13:29:57 +0200

    Remove hc version

--- resources/views/elements.blade.php
+++ resources/views/elements.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Bar Assistant &middot; API Documentation</title>
-    <script src="https://unpkg.com/@stoplight/elements@8.3.4/web-components.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements@8.3.4/styles.min.css">
+    <script src="https://unpkg.com/@stoplight/elements/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/@stoplight/elements/styles.min.css">
     <link rel="icon" type="image/png" href="https://barassistant.app/img/favicon.png">
 </head>
 <body>
