32d3e4ebae950996fdd897dba76a64c1e292019c
Karlo Mikuš <karlo.mikus1@gmail.com>
2024-11-21 19:08:36 +0100

    Use thumb image in menu

--- app/Http/Resources/MenuPublicResource.php
+++ app/Http/Resources/MenuPublicResource.php
@@ -37,7 +37,7 @@ class MenuPublicResource extends JsonResource
                             'public_id' => $menuCocktail->cocktail->public_id,
                             'name' => $menuCocktail->cocktail->name,
                             'short_ingredients' => $menuCocktail->cocktail->getIngredientNames(),
-                            'image' => $menuCocktail->cocktail->getMainImageUrl(),
+                            'image' => $menuCocktail->cocktail->getMainImageThumbUrl(),
                         ];
                     }),
                 ];
