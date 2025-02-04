fea2950d5bd6ba83ff81517f0d5fb8062a4daaaf
Karlo Mikuš <karlo.mikus1@gmail.com>
2025-02-04 20:42:01 +0100

    Fill path for existing ingredient parent relations

--- database/migrations/2025_02_03_203052_add_materialized_path_to_ingredients.php
+++ database/migrations/2025_02_03_203052_add_materialized_path_to_ingredients.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,8 +13,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('ingredients', function (Blueprint $table) {
-            $table->string('materialized_path')->nullable()->index();
+            $table->string('materialized_path')->nullable()->index('ing_path_index');
         });
+
+        DB::select("UPDATE ingredients SET materialized_path = parent_ingredient_id || '/' WHERE parent_ingredient_id IS NOT NULL");
     }
 
     /**
@@ -22,6 +25,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('ingredients', function (Blueprint $table) {
+            $table->dropIndex('ing_path_index');
             $table->dropColumn('materialized_path');
         });
     }
