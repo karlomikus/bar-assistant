2ca1b7af3ff8ac976476c9cb1a767f7a16fc6828
Karlo Mikuš <karlo.mikus1@gmail.com>
2025-02-05 17:08:51 +0100

    Add migration steps

--- database/migrations/2025_02_03_203052_add_materialized_path_to_ingredients.php
+++ database/migrations/2025_02_03_203052_add_materialized_path_to_ingredients.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,7 +18,34 @@ return new class extends Migration
             $table->string('materialized_path')->nullable()->index('ing_path_index');
         });
 
-        DB::select("UPDATE ingredients SET materialized_path = parent_ingredient_id || '/' WHERE parent_ingredient_id IS NOT NULL");
+        // Migrate existing ingredient categories to ingredients
+        // Update materialized path for ingredients with ingredient category
+        $categories = DB::table('ingredient_categories')->get();
+        foreach ($categories as $category) {
+            $userId = DB::table('bars')->select('created_user_id')->where('id', $category->bar_id)->first()->created_user_id;
+            $newIngredientId = DB::table('ingredients')->insertGetId([
+                'name' => $category->name,
+                'slug' => Str::slug($category->name) . '-' . $category->bar_id,
+                'description' => $category->description,
+                'bar_id' => $category->bar_id,
+                'created_at' => $category->created_at,
+                'updated_at' => $category->updated_at,
+                'created_user_id' => $userId,
+            ]);
+
+            DB::table('ingredients')
+                ->where('bar_id', $category->bar_id)
+                ->where('ingredient_category_id', $category->id)
+                ->update(['materialized_path' => $newIngredientId . '/']);
+        }
+
+        // Update materialized path for ingredients with existing parent ingredient
+        DB::select("UPDATE ingredients SET materialized_path = materialized_path || parent_ingredient_id || '/' WHERE parent_ingredient_id IS NOT NULL");
+
+        // Schema::table('ingredients', function (Blueprint $table) {
+        //     $table->dropColumn('ingredient_category_id');
+        // });
+        // Schema::dropIfExists('ingredient_categories');
     }
 
     /**
