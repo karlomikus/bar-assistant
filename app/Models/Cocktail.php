5ff46c64b9dd86e7494f6e0b4fb3f49fd6bfd199
Karlo Mikuš <karlo.mikus1@gmail.com>
2025-03-17 15:50:43 +0100

    Revert

--- app/Models/Cocktail.php
+++ app/Models/Cocktail.php
@@ -93,7 +93,7 @@ class Cocktail extends Model implements UploadableInterface, IsExternalized
      */
     public function tags(): BelongsToMany
     {
-        return $this->belongsToMany(Tag::class)->where('bar_id', $this->bar_id);
+        return $this->belongsToMany(Tag::class);
     }
 
     /**
