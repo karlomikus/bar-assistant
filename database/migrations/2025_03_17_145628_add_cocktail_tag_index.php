832faa2cb612f5d6ed1fc3f1cdae39be34899365
Karlo Mikuš <karlo.mikus1@gmail.com>
2025-03-17 15:57:55 +0100

    Add index to cocktail_id in cocktail_tag table

---
+++ database/migrations/2025_03_17_145628_add_cocktail_tag_index.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cocktail_tag', function (Blueprint $table) {
+            $table->index('cocktail_id', 'cocktail_tag_cocktail_id_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cocktail_tag', function (Blueprint $table) {
+            $table->dropIndex('cocktail_tag_cocktail_id_idx');
+        });
+    }
+};
