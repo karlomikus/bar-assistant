b22342f478ae30c2d74eb2e53020336bffd3050b
Karlo Mikus <contact@karlomikus.com>
2025-07-18 16:26:03 +0200

    Remove flakey test

--- tests/Feature/Http/ImportControllerTest.php
+++ tests/Feature/Http/ImportControllerTest.php
@@ -12,19 +12,6 @@ class ImportControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_import_scrapes_from_url(): void
-    {
-        $membership = $this->setupBarMembership();
-        $this->actingAs($membership->user);
-
-        $this->withHeader('Bar-Assistant-Bar-Id', (string) $membership->bar_id);
-        $response = $this->postJson('/api/import/scrape', [
-            'source' => 'https://barassistant.app'
-        ]);
-
-        $response->assertSuccessful();
-    }
-
     public function test_import_cocktail(): void
     {
         $membership = $this->setupBarMembership();
