676fb9ab961db979f7e99101b3ba0768c8272d8a
Karlo Mikus <contact@karlomikus.com>
2026-01-25 14:31:44 +0100

    update rector paths

--- rector.php
+++ rector.php
@@ -7,10 +7,7 @@ use Rector\Config\RectorConfig;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
         __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
