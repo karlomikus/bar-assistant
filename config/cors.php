2c99d5fb3dbe9aaf6519b947e7bcb82a025d5a6b
Karlo Mikus <contact@karlomikus.com>
2026-08-23 15:22:10 +0200

    Expose location header

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'mcp/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
@@ -25,7 +25,7 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['location'],
 
     'max_age' => 0,
 
