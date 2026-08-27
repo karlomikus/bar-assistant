23a0f5a8be4dc85d03c9f94821851a42ee2facaf
Karlo Mikus <contact@karlomikus.com>
2026-08-27 20:30:00 +0200

    Use indexes for query perf

--- app/Http/Filters/GlassQueryFilter.php
+++ app/Http/Filters/GlassQueryFilter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kami\Cocktail\Http\Filters;
 
+use Kami\Cocktail\Models\Cocktail;
 use Kami\Cocktail\Models\Glass;
 use Spatie\QueryBuilder\QueryBuilder;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -23,7 +24,12 @@ final class GlassQueryFilter extends QueryBuilder
             ])
             ->defaultSort('name')
             ->allowedSorts('name', 'created_at')
-            ->withCount('cocktails')
+            ->addSelect([
+                'cocktails_count' => Cocktail::query()
+                    ->selectRaw('count(*)')
+                    ->whereColumn('glasses.id', 'cocktails.glass_id')
+                    ->where('cocktails.bar_id', bar()->id)
+            ])
             ->with('images')
             ->filterByBar();
     }
